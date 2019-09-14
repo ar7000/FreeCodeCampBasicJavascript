9ff75e551b96037c4a6726c4bfd594c734046f2a
Ash Robinson <a_p_robinson@icloud.com>
2019-09-14 08:24:40 +0100

    Completed initial tasks on JS arrays.

---
+++ 36-StoreMultipleValuesInAVariableWithArrays.js
@@ -0,0 +1,4 @@
+// Example
+var ourArray = ["John", 23];
+
+var myArray = ["Austin", 3.16];
---
+++ 37-NestOneArrayWithinAnother.js
@@ -0,0 +1,4 @@
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+var myArray = [["Nested array part...", 1], ["Nested array part...", 2]];
